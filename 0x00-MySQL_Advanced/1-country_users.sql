922b9c3b2a8c2632d478f80274038fba58cc1be9
rotex <dantejake48@gmail.com>
2023-05-03 21:45:30 +0100

    creating a table, using enumeration

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,15 @@
+-- Script that creates a table users following these requirements:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- country, enumeration of countries: US, CO and TN,
+-- never null (= default will be the first element of
+-- the enumeration, here US)
+
+CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
+  PRIMARY KEY (id)
+);
