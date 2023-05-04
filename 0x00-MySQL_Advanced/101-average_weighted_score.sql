a1e02765732c50c056d6beda01b936b7b785ba59
rotex <dantejake48@gmail.com>
2023-05-04 23:48:52 +0100

    query fix

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -7,14 +7,14 @@ DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER$$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-  UPDATE users, 
+  UPDATE users AS U, 
       (SELECT users.id, SUM(score * weight) / SUM(weight) AS weight_avg 
       FROM users 
       JOIN corrections ON users.id=corrections.user_id 
       JOIN projects ON corrections.project_id=projects.id 
       GROUP BY users.id)
   AS AW
-  SET users.average_score = AW.weight_avg
-  WHERE users.id=AW.id;
+  SET U.average_score = AW.weight_avg
+  WHERE U.id=AW.id;
 END$$
 DELIMITER;
