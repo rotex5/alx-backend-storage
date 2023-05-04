9ea6241f457eae26bf01d1530fd0e1fa9116e011
rotex <dantejake48@gmail.com>
2023-05-05 00:12:53 +0100

    remove shortcuts

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -9,7 +9,7 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
 BEGIN
     UPDATE users set average_score = (
     SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
-    FROM corrections as C ON
-    INNER JOIN projects AS P ON P.id = C.project_id
-    where C.user_id = users.id);
+    FROM corrections
+    INNER JOIN projects ON projects.id = corrections.project_id
+    where corrections.user_id = users.id);
 END;
