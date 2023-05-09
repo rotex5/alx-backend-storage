829b026ae81ccc020eca874a77d5eea6c8d33441
rotex <dantejake48@gmail.com>
2023-05-09 16:28:43 +0100

    inserts a new document in a collection based on kwargs

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -8,5 +8,5 @@ def insert_school(mongo_collection, **kwargs):
     """
     inserts a new document in a collection based on kwargs
     """
-    id = mongo_collection.insert_one(kwargs).inserted_id
-    return id
+    _id = mongo_collection.insert_one(kwargs).inserted_id
+    return _id
