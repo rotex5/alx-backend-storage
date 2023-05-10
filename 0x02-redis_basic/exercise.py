462af5f196a40a62c4d111ca1d718e4953abc156
rotex <dantejake48@gmail.com>
2023-05-10 14:23:26 +0100

    code refactor

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 Redis basic
 """
 import redis
-from uuid import uuid4
+import uuid
 from typing import Union, Optional, Callable
 
 
@@ -12,37 +12,46 @@ class Cache:
     Class implementation of a cache
     """
     def __init__(self):
-        """Initaillizing cache variable"""
+        """
+        Initializes a cache instance with the given Redis
+        connection. If no connection is provided, a new
+        connection is created with default options.
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ return generated random key"""
-        key = str(uuid4())
+        """
+        Stores the given data in Redis and returns a randomly
+        generated key to retrieve it.
+        """
+        key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
     def get(self, key: str,
             fn: Optional[Callable] = None
             ) -> Union[str, bytes, int, float]:
-        """ returns original data """
-        value = self._redis.get(key)
+        """ Returns the value associated with the given key from
+        Redis, optionally transformed by the given function."""
+        val = self._redis.get(key)
         if fn:
-            value = fn(value)
-        return value
+            val = fn(val)
+        return val
 
     def get_str(self, key: str) -> str:
-        """ Parameterizes cache.get with the correct
-        converion from redis value to str """
-        value = self._redis.get(key)
-        return value.decode("utf-8")
+        """ Returns the string value associated with the
+        given key from Redis."""
+        val = self._redis.get(key)
+        return val.decode("utf-8")
 
     def get_int(self, key: str) -> int:
-        """ Parameterizes chache.get with the correct
-        conversion from redis value to int """
-        value = self._redis.get(key)
+        """ Returns the integer value associated with the given
+        key from Redis, or zero if the key is not present or
+        the value is not a valid integer."""
+        val = self._redis.get(key)
         try:
-            value = int(value.decode("utf-8"))
+            val = int(val.decode("utf-8"))
         except Exception:
-            value = 0
-        return value
+            val = 0
+        return val
