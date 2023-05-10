e3ac3d7b94d8fee83b80e584c06172ae55b34dc4
rotex <dantejake48@gmail.com>
2023-05-10 14:02:24 +0100

    implementing get, get_str and get_int methods to return differe values from redis db based on methods called

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Redis basic
 """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -21,3 +21,28 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float]:
+        """ returns original data """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """ Parameterizes cache.get with the correct
+        converion from redis value to str """
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """ Parameterizes chache.get with the correct
+        conversion from redis value to int """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
